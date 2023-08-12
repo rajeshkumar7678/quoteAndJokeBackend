17b37c462c604195a684703738cf02f7ff29456d
Rajesh kumar <rk129479@gmail.com>
2023-08-12 12:41:52 +0530

    done

--- routes/quoteroute.js
+++ routes/quoteroute.js
@@ -42,6 +42,39 @@ genquote.get('/quote', async (req, res) => {
     }
 });
 
+genquote.get('/quote', async (req, res) => {
+  try {
+      let {prompt}=req.query
+      const openAi = new OpenAIApi(
+          new Configuration({
+            apiKey: process.env.apikey,
+          })
+      );
+      
+      const response = await openAi.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [
+              {
+                "role": "user",
+                "content": `joke for ${prompt}`
+              }
+              
+            ],
+          max_tokens: 1000,
+          temperature: 1,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+      });
+       
+      const reply = response.data.choices[0].message.content.trim()
+      res.send({"quote":reply})
+  } catch (error) {
+    console.error('Error fetching quote:', error.message);
+    res.status(500).json({ error: 'Failed to fetch quote.' });
+  }
+});
+
   
 
 
